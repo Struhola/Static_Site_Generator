f4d50fae4c1a59f30c223b6c4255523012c0aa39
Struhola <Michal_For@wp.pl>
2025-09-26 16:41:42 +0200

    CH:2 Nodes L4: Leaf Node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,12 +1,13 @@
-
-class HTMLNode():
-    def __init__(self, tag = None, value = None, children = None, props = None):
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
+
     def to_html(self):
-        raise NotImplementedError("to_html method not yet implemented.")
+        raise NotImplementedError("to_html method not implemented")
+
     def props_to_html(self):
         if self.props is None:
             return ""
@@ -14,6 +15,21 @@ class HTMLNode():
         for prop in self.props:
             props_html += f' {prop}="{self.props[prop]}"'
         return props_html
+
     def __repr__(self):
-        print(f"Tag: {self.tag} | Value: {self.value} | Children: {self.children} | props: {self.props}")
-        
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
