449c8ee4b860127e12f031149ac1c911bd8937e0
Struhola <Michal_For@wp.pl>
2025-09-26 17:02:41 +0200

    CH2: Nodes L6: Text Node to HTML Node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,18 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+        
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag required!")
+        if self.children is None:
+            raise ValueError("Children required!")
+        str_children =""
+        for child in self.children:
+            str_children+= child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{str_children}</{self.tag}>"
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,13 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    TEXT = "Plain text"
-    BOLD = "**text**"
-    ITALIC = "_text_"
-    CODE = "`code`"
-    LINK = "[anchor text](url)"
-    IMAGE = "[alt text](url)"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
     
 class TextNode:
     def __init__(self, TEXT, TEXT_TYPE, URL = None):
@@ -25,3 +26,22 @@ class TextNode:
             self.text_type == other.text_type and
             self.url == other.url
         )
+        
+def text_node_to_html_node(node):
+    match node.text_type:
+        case TextType.BOLD:
+            return LeafNode("b", node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", node.text)
+        case TextType.CODE:
+            return LeafNode("code", node.text)
+        case TextType.LINK:
+            props = "" if node.url is None else node.url
+            return LeafNode("a", node.text,{"href": props})
+        case TextType.IMAGE:
+            url = "" if node.url is None else node.url
+            props = "" if node.text is None else node.text
+            return LeafNode("img", "", {"src": url, "alt": props})
+        case TextType.TEXT:
+            return LeafNode(None, node.text)
+    raise Exception("Unknown TextType")
