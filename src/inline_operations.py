788df9527d700f4a24120659fda7f23c4e2f3fc8
Struhola <Michal_For@wp.pl>
2025-09-27 09:39:33 +0200

    CH3: Inline L5: Split Images and Links

--- src/inline_operations.py
+++ src/inline_operations.py
@@ -29,3 +29,49 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)]\(([^()]*)\)", text)
     return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        old_node_text = old_node.text
+        image_url_tuples_list = extract_markdown_images(old_node_text)
+        if len(image_url_tuples_list) == 0:
+            new_nodes.append(old_node)
+            continue
+        for img_url_tuple in image_url_tuples_list:
+            parts = old_node_text.split(f"![{img_url_tuple[0]}]({img_url_tuple[1]})", 1)
+            if len(parts) != 2:
+                raise ValueError("invalid markdown, formatted image not closed")
+            if parts[0] != "":
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(img_url_tuple[0], TextType.IMAGE, img_url_tuple[1]))
+            old_node_text = parts[1]
+        if old_node_text != "":
+            new_nodes.append(TextNode(old_node_text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        old_node_text = old_node.text
+        alt_link_tuples_list = extract_markdown_links(old_node_text)
+        if len(alt_link_tuples_list) == 0:
+            new_nodes.append(old_node)
+            continue
+        for alt_link_tuple in alt_link_tuples_list:
+            parts = old_node_text.split(f"[{alt_link_tuple[0]}]({alt_link_tuple[1]})", 1)
+            if len(parts) != 2:
+                raise ValueError("invalid markdown, formatted link not closed")
+            if parts[0] != "":
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(alt_link_tuple[0], TextType.LINK, alt_link_tuple[1]))
+            old_node_text = parts[1]
+        if old_node_text != "":
+            new_nodes.append(TextNode(old_node_text, TextType.TEXT))
+    return new_nodes
