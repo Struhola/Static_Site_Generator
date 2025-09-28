e4ededce73db009312883f1a84554592ed8ac3e0
Struhola <Michal_For@wp.pl>
2025-09-28 09:58:57 +0200

    CH5: Website L1: Copy Static

--- src/main.py
+++ src/main.py
@@ -1,8 +1,32 @@
 #./main.sh
 
-from textnode import TextNode, TextType
+import os
+import shutil
+import sys
+from file_operations import copy_contents
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+#"./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_basepath = "/"
+
+
 def main():
-    object = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(object)
-    
-main()
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    if not os.path.exists(dir_path_static):
+        raise Exception(f"Source {dir_path_static} doesn't exists")
+
+    if os.path.exists(dir_path_public):
+        print(f"Destination {dir_path_public} already exists - REMOVING")
+        shutil.rmtree(dir_path_public)
+
+    copy_contents(dir_path_static, dir_path_public)
+
+if __name__ == "__main__":
+    main()
