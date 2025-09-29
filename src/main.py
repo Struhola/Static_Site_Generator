363d8212b5afa4ab5f9451c6fef3499edf6c7719
Struhola <Michal_For@wp.pl>
2025-09-29 10:45:04 +0200

    CH5: Website L5: Deploy to the internet

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from generate_operations import generate_page, generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 #"./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
@@ -16,6 +16,11 @@ default_basepath = "/"
 
 
 def main():
+    if sys.argv[1] is None:
+        basepath = default_basepath
+    else:
+        basepath = sys.argv[1]
+        
     if not os.path.exists(dir_path_static):
         raise Exception(f"Source {dir_path_static} doesn't exists")
 
@@ -25,7 +30,7 @@ def main():
 
     copy_contents(dir_path_static, dir_path_public)
     print("Generating page...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
     
 if __name__ == "__main__":
     main()
